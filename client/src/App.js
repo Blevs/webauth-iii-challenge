f7ed93ffde94561dd9d146f4e333de51c107dc6f
Henry Blevins <heblevi@gmail.com>
2019-05-09 16:06:27 -0400

    Initialize client and build signup

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route, NavLink } from 'react-router-dom';
+import SignUp from './views/SignUp';
+// import SignIn from './views/SignIn';
+// import Users from './views/Users';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <NavLink to="/signup">Sign Up</NavLink>
+        <NavLink to="/signin">Sign In</NavLink>
+        <NavLink to="/users">Users</NavLink>
+      </header>
+      <Route path="/signup" component={SignUp}/>
+      {/* <Route path="/signin" component={SignIn}/> */}
+      {/* <Route path="/users" component={users}/> */}
+    </div>
+  );
+}
+
+export default App;
