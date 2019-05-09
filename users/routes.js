e66af82b0154892ba5a18ccf4ea38d9b8d43d764
Henry Blevins <heblevi@gmail.com>
2019-05-09 12:52:32 -0400

    Protect users route
    Have route return passwords, as specified in readme

--- users/routes.js
+++ users/routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const Users = require('./model.js');
+const protected = require('../auth/protected-middleware.js');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', protected, (req, res) => {
   Users.get()
-    .then(users => users.forEach(u => u.password = undefined)
-          || res.status(200).json(users))
+    .then(users =>
+          // users.forEach(u => u.password = undefined) ||
+          res.status(200).json(users))
     .catch(() => res.status(500).json({
       message: "Error fetching users"
     }));
