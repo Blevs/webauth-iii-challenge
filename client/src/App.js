8b64e4a1a2105444e5d2619554d0f259e5423eff
Henry Blevins <heblevi@gmail.com>
2019-05-09 16:15:11 -0400

    Add signout button

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, withRouter } from 'react-router-dom';
 import SignUp from './views/SignUp';
 import SignIn from './views/SignIn';
 import Users from './views/Users';
 import './App.css';
 
-function App() {
+function App(props) {
+  const signout = e => {
+    e.preventDefault();
+    localStorage.removeItem('jwt');
+    props.history.push('/signin');
+  };
   return (
     <div className="App">
       <header>
         <NavLink to="/signup">Sign Up</NavLink>
         <NavLink to="/signin">Sign In</NavLink>
         <NavLink to="/users">Users</NavLink>
+        <button onClick={signout}>Sign Out</button>
       </header>
       <Route path="/signup" component={SignUp}/>
       <Route path="/signin" component={SignIn}/>
@@ -20,4 +26,4 @@ function App() {
   );
 }
 
-export default App;
+export default withRouter(App);
